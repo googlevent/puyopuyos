f13618f7a30bd591d54f3e30a7e62ae1df645cf8
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2015-12-22 00:03:07 +0900

    画像選択部分の更新
    縦スクロールにしました！

--- js/imagepack.js
+++ js/imagepack.js
@@ -5,7 +5,7 @@ function imload(imageid){
 
 $( function(){
 		for(var i=1; i<10; i++){
-			var des = '<li><img class=\"imbef\" src=\"./img/hakodot'+i+'.png\" style=\"margin: 0;padding: 0 0 3px 3px;\" onclick=\"imload('+i+')\"></li>';
+			var des = '<li><img class=\"imbef\" src=\"./img/hakodot'+i+'.png\" style=\"margin: 0;padding: 0 0 8px 3px;\" onclick=\"imload('+i+')\"></li>';
 			$('#content #preference_rank #imagepack .aft ul #aftul').append(des);
 		}
 });
