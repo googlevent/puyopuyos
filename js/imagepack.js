6f0b48da315f62d27d6403e3e5e9b774ef561e67
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2015-12-21 17:57:21 +0900

    imagepack.jsの更新・画像の追加
    imagepack.jsの更新・画像の追加

--- js/imagepack.js
+++ js/imagepack.js
@@ -3,9 +3,13 @@ $( function(){
 	"./img/hakodot2.png",
 	"./img/hakodot3.png",
 	"./img/hakodot4.png",
-	"./img/hakodot5.png"];
+	"./img/hakodot5.png",
+	"./img/hakodot6.png",
+	"./img/hakodot7.png",
+	"./img/hakodot8.png",
+	"./img/hakodot9.png"];
 	for(var i=0; i<imageFile.length; i++){
-		var des = '<li><img class=\"imbef\" src=\"'+imageFile[i]+'\" style=\"margin: 0;padding:3px\"></li>';
+		var des = '<li><img class=\"imbef\" src=\"'+imageFile[i]+'\" style=\"margin: 0;padding: 0 0 3px 3px;\"></li>';
 		$('#content #preference_rank #imagepack .aft ul #aftul').append(des);
 	}
 });
