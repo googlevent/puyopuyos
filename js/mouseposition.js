0a3f980d8e0483dc3ae4c5945e01e51b91e4cc3a
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2015-12-22 20:53:25 +0900

    top pageの追加
    top.htmlをつくりました。未完成なので雰囲気だけ。。

---
+++ js/mouseposition.js
@@ -0,0 +1,21 @@
+var mouseX, mouseY;
+function MouseMove00Func(e){
+	var element;
+	mouseX = e.screenX;
+	// console.log(mouseX);
+	mouseY = e.screenY;
+	var m = mouseX-100;
+	 // src: "/images/hat.gif"
+	 $('#droid').attr({style: 'left:'+m});
+	}
+
+	if(document.addEventListener){
+
+		// マウスを移動するたびに実行されるイベント
+		document.addEventListener("mousemove" , MouseMove00Func);
+
+		// アタッチイベントに対応している
+	}else if(document.attachEvent){
+		// マウスを移動するたびに実行されるイベント
+		document.attachEvent("onmousemove" , MouseMove00Func);
+	}
