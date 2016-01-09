8dc4838f2cf007e8dc3f077cc3b5da9a70aa5bcb
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2016-01-09 10:14:57 +0900

    プレイヤー画像をまとめました
    プレイヤー画像をまとめました

--- js/imagepack.js
+++ js/imagepack.js
@@ -1,11 +1,11 @@
 function imload(imageid){
-	document.getElementById("playerimg").innerHTML='<img src=\"./img/hakodot'+imageid+'.png\"></div>';
+	document.getElementById("playerimg").innerHTML='<img src=\"./img/car/hakodot'+imageid+'.png\"></div>';
 		// console.log(imageid);
 }
 
 $( function(){
-		for(var i=1; i<10; i++){
-			var des = '<li><img class=\"imbef\" src=\"./img/hakodot'+i+'.png\" style=\"margin: 0;padding: 0 0 8px 3px;\" onclick=\"imload('+i+')\"></li>';
+		for(var i=1; i<13; i++){
+			var des = '<li><img class=\"imbef\" src=\"./img/car/hakodot'+i+'.png\" style=\"margin: 0;padding: 0 0 8px 3px;\" onclick=\"imload('+i+')\"></li>';
 			$('#content #preference_rank #imagepack .aft ul #aftul').append(des);
 		}
 });
