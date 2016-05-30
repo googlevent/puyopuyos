d335efc283c953df727f81a5d8271b7521419d2b
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2016-05-30 23:47:50 +0900

    微修正

--- js/imagepack.js
+++ js/imagepack.js
@@ -2,7 +2,6 @@ function imload(imageid){
 	document.getElementById("playerimg").innerHTML='<img src=\"./img/car/hakodot'+imageid+'.png\"></div>';
 		// console.log(imageid);
 }
-
 $( function(){
 		for(var i=1; i<13; i++){
 			var des = '<li><img class=\"imbef\" src=\"./img/car/hakodot'+i+'.png\" style=\"margin: 0;padding: 0 0 8px 3px;\" onclick=\"imload('+i+')\"></li>';
